8c1177588d5c52354210a30158bf3b8f615fc0b2
raula09 <163653632+raula09@users.noreply.github.com>
2024-11-10 18:49:16 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,206 @@
+﻿using CarMarket;
+
+User loggedInUser = null;  
+while (true)
+{
+    Console.WriteLine("Select an option: ");
+    Console.WriteLine("1. Register");
+    Console.WriteLine("2. Login");
+    Console.WriteLine("3. Exit");
+    string option = Console.ReadLine();
+
+    User user = new User();
+    switch (option)
+    {
+
+        case "1":
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Password: ");
+            string password = Console.ReadLine();
+
+            
+
+            Console.Write("Enter Role (admin/user): ");
+            string role = Console.ReadLine();
+            
+            Console.WriteLine("enter age");
+            int age = int.Parse(Console.ReadLine());
+            
+
+            User.Register(name, password, role, age);
+        
+           
+            loggedInUser = user.Login(name, password);
+
+         
+            if (loggedInUser != null)
+            {
+                if (loggedInUser.Role == "admin")
+                {
+                    AdminMenu();
+                }
+                else if (loggedInUser.Role == "user")
+                {
+                    UserMenu(loggedInUser);
+                }
+            }
+            break;
+
+        case "2":
+            Console.Write("Enter Name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter Password: ");
+            password = Console.ReadLine();
+
+            
+            loggedInUser = user.Login(name, password);
+
+            if (loggedInUser != null)
+            {
+                if (loggedInUser.Role == "admin")
+                {
+                    AdminMenu();
+                }
+                else if (loggedInUser.Role == "user")
+                {
+                    UserMenu(loggedInUser);
+                }
+            }
+            break;
+
+        case "3":
+            Console.WriteLine("Bye Bye");
+            return;
+
+        default:
+            Console.WriteLine("Invalid");
+            break;
+    }
+}
+        
+
+   
+        static void AdminMenu()
+{
+    while (true)
+    {
+        Console.WriteLine("Admin Options: ");
+        Console.WriteLine("1: Add Car");
+        Console.WriteLine("2: Remove Car");
+        Console.WriteLine("3: Display All Cars");
+        Console.WriteLine("4: Log Out");
+
+        string adminOption = Console.ReadLine();
+
+        switch (adminOption)
+        {
+            case "1":
+                Car.AddCar();
+                break;
+
+            case "2":
+
+                Console.WriteLine("Enter the car ID to remove: ");
+                int removeCarId = int.Parse(Console.ReadLine());
+                Car.RemoveCar(removeCarId);
+                break;
+
+            case "3":
+                Car.DisplayAllCars();
+                break;
+
+            case "4":
+                Console.WriteLine("Bye bye");
+                return;
+
+            default:
+                Console.WriteLine("Invalid");
+                break;
+        }
+    }
+}
+
+static void UserMenu(User loggedInUser)
+{
+    Car carManager = new Car();
+    while (true)
+    {
+        Console.WriteLine("User Options: ");
+        Console.WriteLine("1: Buy Car");
+        Console.WriteLine("2: My Car");
+        Console.WriteLine("3: Display All Cars");
+        Console.WriteLine("4: Check Balance");
+        Console.WriteLine("5: Sell My Car");
+        Console.WriteLine("6: Sort Price By Ascending");
+        Console.WriteLine("7: Sort Price By Descending");
+        Console.WriteLine("8: Log Out");
+
+        string userOption = Console.ReadLine();
+
+        switch (userOption)
+        {
+            case "1":
+                Car.DisplayAllCars();
+                Console.WriteLine("Enter the car ID to buy: ");
+                int carId = int.Parse(Console.ReadLine());
+
+                Car carToBuy = Car.GetCarById(carId);
+
+                if (carToBuy != null)
+                {
+                    loggedInUser.BuyCar(carToBuy);
+                }
+                else
+                {
+                    Console.WriteLine("Car not found.");
+                }
+                break;
+
+            case "2":
+                Console.WriteLine("Your Cars: ");
+                
+                loggedInUser.DisplayPurchasedCars(); 
+                break;
+
+            case "3":
+                Car.DisplayAllCars();
+                break;
+
+            case "4":
+                loggedInUser.CheckBalance();
+                break;
+            case "5":
+                Console.WriteLine("Your Cars: ");
+
+                loggedInUser.DisplayPurchasedCars();
+                Console.WriteLine("Enter the car ID to sell: ");
+                int carId2 = int.Parse(Console.ReadLine());
+                Car carToSell = Car.GetCarById(carId2);
+                if (carToSell != null)
+                {
+                    loggedInUser.SellCar(carToSell);
+                }
+                else
+                {
+                    Console.WriteLine("Car not found.");
+                }
+                break;
+                case "6":
+                carManager.SortByAsc();
+                break;
+            case "7":
+                carManager.SortByDesc();
+                break;
+            case "8":
+                Console.WriteLine("Logging out...");
+                return;
+
+            default:
+                Console.WriteLine("Invalid option. Please try again.");
+                break;
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarMarket
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+        public int Age { get; set; }
+        public int Balance { get; set; } = 50000;
+
+        public static List<User> Users { get; set; } = new List<User>();
+        
+        public List<Car> PurchasedCars { get; set; } = new List<Car>();
+        public override string ToString()
+        {
+            return $"{Name} {Password} {Role} {Balance} {Age}";
+        }
+
+        public void CheckBalance()
+        {
+            Console.WriteLine($"Your balance is: {Balance} $");
+        }
+
+        
+        public static void Register(string name, string password, string role, int age)
+        {
+
+            if (age <= 18)
+            {
+                throw new Exception($"exception: you are under 18");
+            }
+            else {
+                Users.Add(new User { Name = name, Password = password, Role = role, Age = age });
+            Console.WriteLine("Registered successfully.");
+            }
+            
+
+        }
+        public void WriteFile(string content)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\users.txt";
+            using (FileStream fs = new FileStream(path, FileMode.Append, FileAccess.Write))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine(content);
+                }
+            }
+        }
+
+        public void ReadFile()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\users.txt";
+            if (File.Exists(path))
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        Console.WriteLine(sr.ReadToEnd());
+                    }
+                }
+            }
+        }
+
+        public  User Login(string name, string password)
+        {
+            var user = Users.FirstOrDefault(x => x.Name == name && x.Password == password);
+            Console.WriteLine(user.ToString());
+            WriteFile(user.ToString());
+            if (user != null)
+            {
+                Console.WriteLine($"Logged in as {user.Role}");
+                return user;  
+            }
+            else
+            {
+                Console.WriteLine("Incorrect username or password.");
+                return null;
+            }
+        }
+        public void BuyCar(Car car)
+        {
+            if (Balance < car.Price)
+            {
+                throw new CarException($"you dont have enough money for {car.Model}. You need atleast {car.Price - Balance}");
+            }
+
+            Balance -= car.Price;
+            PurchasedCars.Add(car);
+            Console.WriteLine($"You have bought the car {car.Model} for {car.Price}. Your balance = {Balance}.");
+        }
+
+        public void SellCar(Car car)
+        {
+            if (!PurchasedCars.Contains(car))
+            {
+                throw new CarException($"Exception: you dont own {car.Model}");
+            }
+
+            Balance += car.Price;
+            PurchasedCars.Remove(car);
+            Console.WriteLine($"You have sold the car {car.Model} for {car.Price}. Your new balance is {Balance}.");
+        }
+
+        public void DisplayPurchasedCars()
+        {
+            if (PurchasedCars.Count == 0)
+            {
+                Console.WriteLine("You have not purchased any cars.");
+            }
+            else
+            {
+                Console.WriteLine("Purchased Cars:");
+                foreach (var car in PurchasedCars)
+                {
+                    Console.WriteLine($"Car Model: {car.Model}, Price: {car.Price}, Year: {car.Year}, Manufacturer: {car.Manufacturer}");
+                }
+            }
+        }
+
+     
+    }
+}
